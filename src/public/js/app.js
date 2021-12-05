7d87653e1a63c5a7bdc2d6186d8911810092c3e3
김소연 <soyeon.dev@gmail.com>
2021-12-05 22:12:32 +0900

    n브라우저 간 통신 기능 개발 완료

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,51 @@
-alert("Hi~")
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+function makeMessage(type, payload){
+    const msg = {type, payload}
+    return JSON.stringify(msg)
+}
+
+function handleOpen(){
+    console.log("Connected to Server 🥕 ")
+}
+
+socket.addEventListener("open", () => handleOpen)
+
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data, " from the Server ")
+    const li = document.createElement("li")
+    li.innerText = message.data
+    messageList.append(li)
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconneted from Server ❌ ")
+})
+
+// handle submit the message 
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input")
+    socket.send(
+        makeMessage("new_message", input.value)
+    )
+    const li = document.createElement("li")
+    li.innerText = `You: ${input.value}`
+    messageList.append(li)
+    input.value = ""
+}
+
+// handle submit the nickname 
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input")
+    socket.send(
+        makeMessage("nickname", input.value)
+    )
+}
+
+messageForm.addEventListener("submit", handleSubmit)
+nickForm.addEventListener("submit", handleNickSubmit)
--- src/server.js
+++ src/server.js
@@ -1,10 +1,41 @@
+import http from "http";
 import express from "express";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"))
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+const sockets = [];
+
+// WebSocket Connected
+wss.on("connection", (socket) => {
+    sockets.push(socket)
+    socket["nickname"] = "sola"
+    console.log("Connected to Browser 🥕")
+    
+    // WebSocket Disconneted
+    socket.on("close", () => {console.log("Disconnected from the Browser ❌ ")})
+    // WebSocket receive the message
+    socket.on("message", (message) => {
+        const msg = JSON.parse(message)
+        console.log("msg: ", msg)
+        switch(msg.type){
+            case "new_message":
+            sockets.forEach((aSocket) => {
+                aSocket.send(`${socket.nickname}: ${msg.payload}`)
+            })
+            case "nickname":
+                socket["nickname"] = msg.payload;
+        }
+    })
+});
+
+server.listen(3000, handleListen)
