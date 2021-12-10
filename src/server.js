2f6cab353cb82e8cb777fc1d37dcb86825c25ab7
김소연 <soyeon.dev@gmail.com>
2021-12-11 07:01:15 +0900

    user count 기능 추가

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
+import { count } from "console";
  
 const app = express();
 
@@ -24,6 +25,11 @@ function publicRooms(){
     return publicRooms
 }
 
+function countRoom(roomName){
+    console.log("roomName: ", roomName)
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size
+}
+
 wsServer.on("connection", socket => {
     socket["nickname"] = "Anon"
     socket.onAny((event) => {   // 모든 이벤트를 살핌
@@ -33,12 +39,12 @@ wsServer.on("connection", socket => {
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName)
         done();
-        socket.to(roomName).emit("welcome", socket.nickname)
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName))
         wsServer.sockets.emit("room_change", publicRooms())
     });
     socket.on("disconnecting", () => {  // socket이 방을 떠나기 바로 직전에 발생
         socket.rooms.forEach(room => {
-            socket.to(room).emit("bye", socket.nickname);            
+            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);            
         })
 
     }) 
