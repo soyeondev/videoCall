3f1b8f48c3199b796b22df24a80cdc57f1861f07
김소연 <soyeon.dev@gmail.com>
2021-12-11 23:33:50 +0900

    initial commit

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import http from "http";
-import SocketIO from "socket.io";
+// import SocketIO from "socket.io";
+import {Server} from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
-import { count } from "console";
- 
+
 const app = express();
 
 app.set("view engine", "pug");
@@ -13,7 +14,16 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {  
+    cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true
+  }
+});
+
+instrument(wsServer, {
+    auth: false
+  });
 
 function publicRooms(){
     const {sockets: {adapter: {sids, rooms}}} = wsServer
