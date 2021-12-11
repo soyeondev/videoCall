78ae62f7c6e9e7de0c6c6de76b28a84e8599eba2
김소연 <soyeon.dev@gmail.com>
2021-12-12 00:34:21 +0900

    handle mute and camera click

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
-// import SocketIO from "socket.io";
-import {Server} from "socket.io";
+// import {Server} from "socket.io";
+import SocketIO from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
@@ -11,62 +11,10 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app)
+const wsServer = SocketIO(httpServer)
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
-const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {  
-    cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true
-  }
-});
-
-instrument(wsServer, {
-    auth: false
-  });
-
-function publicRooms(){
-    const {sockets: {adapter: {sids, rooms}}} = wsServer
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if(sids.get(key) === undefined)
-            publicRooms.push(key)
-    })
-    return publicRooms
-}
-
-function countRoom(roomName){
-    console.log("roomName: ", roomName)
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size
-}
-
-wsServer.on("connection", socket => {
-    socket["nickname"] = "Anon"
-    socket.onAny((event) => {   // 모든 이벤트를 살핌
-        // console.log(wsServer.sockets.adapter)
-        console.log(`Socket Event: ${event}`)
-    })
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName)
-        done();
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName))
-        wsServer.sockets.emit("room_change", publicRooms())
-    });
-    socket.on("disconnecting", () => {  // socket이 방을 떠나기 바로 직전에 발생
-        socket.rooms.forEach(room => {
-            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);            
-        })
-
-    }) 
-
-    socket.on("disconnect", () => { // socket이 방을 떠날때 발생
-        wsServer.sockets.emit("room_change", publicRooms())
-    })
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`)
-        done()
-    })
-    socket.on("nickname", nickname => socket["nickname"] = nickname)
-});
 
 httpServer.listen(3000, handleListen)
