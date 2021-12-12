32cf002a71fffda74f69523b0ac4d03a32fd67d2
김소연 <soyeon.dev@gmail.com>
2021-12-12 15:00:39 +0900

    create room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,10 +4,14 @@ const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute")
 const cameraBtn = document.getElementById("camera")
 const cameraSelect = document.getElementById("cameras")
+const call = document.getElementById("call")
+
+call.hidden = true
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let rooName;
 
 async function getCameras(){
     try{
@@ -54,8 +58,6 @@ async function getMedia(deviceId){
     }
 }
 
-getMedia()
-
 function handleMuteClick(){
     console.log(myStream.getAudioTracks())
     myStream.getAudioTracks().forEach(track => {
@@ -94,3 +96,29 @@ async function handleCameraChange(){
 muteBtn.addEventListener("click", handleMuteClick)
 cameraBtn.addEventListener("click", handleCameraClick)
 cameraSelect.addEventListener("input", handleCameraChange)
+
+// Welcome Form(join a room)
+const welcome = document.getElementById("welcome")
+const welcomeForm = welcome.querySelector("form")
+
+function startMedia(){
+    welcome.hidden = true;
+    call.hidden = false;
+    getMedia()
+}
+
+function handleWelcomeSubmit(event){
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input")
+    socket.emit("join_room", input.value, startMedia)
+    rooName = input.value
+    input.value = ""   
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit)
+
+// Socket Code
+socket.on("welcome", () => {
+    console.log("someone joined")
+})
+
