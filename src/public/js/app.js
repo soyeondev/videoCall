1645f69eb7d6c3aa47ba8b15b5b8d9ddc58aaf94
김소연 <soyeon.dev@gmail.com>
2021-12-12 13:37:56 +0900

    get device id

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,29 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute")
 const cameraBtn = document.getElementById("camera")
+const cameraSelect = document.getElementById("cameras")
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+
+async function getCameras(){
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => {
+            return device.kind === "videoinput"
+        })
+        cameras.forEach(camera => {
+            const option = document.createElement("option")
+            option.value = camera.deviceId
+            option.innerText = camera.label
+            cameraSelect.appendChild(option)
+        })
+        console.log(cameras)
+    }catch(e){
+        console.log(e)
+    }
+}
 
 async function getMedia(){
     try{
@@ -14,8 +33,8 @@ async function getMedia(){
             audio: true,
             video: true
         })
-        console.log(myStream)
         myFace.srcObject = myStream;
+        await getCameras();
     } catch(e){
         console.log(e)
     }
@@ -24,6 +43,10 @@ async function getMedia(){
 getMedia()
 
 function handleMuteClick(){
+    console.log(myStream.getAudioTracks())
+    myStream.getAudioTracks().forEach(track => {
+        track.enabled = !track.enabled
+    });
     if(!muted){
         muteBtn.innerText = "Unmute"
         muted = true
@@ -31,9 +54,14 @@ function handleMuteClick(){
         muteBtn.innerText = "Mute"
         muted = false
     }
+    console.log("myStream getAudioTracks: ", myStream.getAudioTracks())
 }
 
 function handleCameraClick(){
+    console.log(myStream.getVideoTracks())
+    myStream.getVideoTracks().forEach(track => {
+        track.enabled = !track.enabled
+    })
     if(cameraOff){
         cameraBtn.innerText = "Turn Camera Off"
         cameraOff = false
@@ -43,5 +71,10 @@ function handleCameraClick(){
     }
 }
 
+function handleCameraChange(){
+    console.log(cameraSelect.value)
+}
+
 muteBtn.addEventListener("click", handleMuteClick)
 cameraBtn.addEventListener("click", handleCameraClick)
+cameraSelect.addEventListener("input", handleCameraChange)
